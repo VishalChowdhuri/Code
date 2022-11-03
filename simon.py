9ac10953a1aca396df1e6c9f97544f69001108fd
VishalChowdhuri <113933584+VishalChowdhuri@users.noreply.github.com>
2022-11-02 22:02:48 -0400

    Update simon.py

--- simon.py
+++ simon.py
@@ -4,18 +4,22 @@ import touchio
 import digitalio as dio
 import neopixel
 import random
-#Creating variable names for colors that will be used throughout the program.
-red = (255, 0 , 0)
-green = (0, 255, 0)
+
+
 blue = (0, 0, 255)
+red = (255, 0 , 0)
 yellow = (255, 255, 0)
 black = (0, 0, 0)
-#Creating the list that will keep track of the colors displayed.
-gencolors = []
+green = (0, 255, 0)
+
+
 num = 0
 np = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=0.5, auto_write=False)
-#Creating variable to determine if the game is over or not.
+
+
 over = True
+gencolors = []
+
 
 abutton = dio.DigitalInOut(board.BUTTON_A)
 abutton.direction = dio.Direction.INPUT
@@ -30,23 +34,53 @@ pad4 = board.A2
 t4 = touchio.TouchIn(pad4)
 led = dio.DigitalInOut(board.LED)
 led.direction = dio.Direction.OUTPUT
-#Both these defs will flash a certain color when called, fgreen will flash green and fred will flash red.
+
+
+‘’’
+Function: fgreen
+Description: This function flashes the color green
+Parameters: None
+Return: None
+‘’’
 def fgreen():
     np.fill(green)
     np.show()
     time.sleep(0.1)
     np.fill(black)
     np.show()
+
+
+‘’’
+Function: fred
+Description: This function flashes the color red
+Parameters: None
+Return: None
+‘’’
 def fred():
     np.fill(red)
     np.show()
     time.sleep(0.1)
     np.fill(black)
     np.show()
-#This function will assign colors to two seperate lights.
+
+
+‘’’
+Function: blink
+Description: This function blinks 2 certain neopixels a certain color.
+Parameters: Color, num, num1, color is the color that will set the 2 neopixels, num and num1 are the two neopixels.
+Return: None
+‘’’
 def blink(num, num1, rgb):
     np[num] = rgb
     np[num1] = rgb
+
+
+‘’’
+Function: show
+Description: This function will take in a number between 1-4 and show a certain color. For instance 1 will show red, 2 will show green, 3 will show blue, and 4 will show yellow.
+Parameters: numcolor, this is the number that will be taken in and compared.
+Return: None
+‘’’
 def show(numcolor):
     if numcolor == 1:
         blink(0,1,red)
@@ -74,6 +108,12 @@ def show(numcolor):
         np.show()
 
 
+‘’’
+Function: nextround
+Description: This function generates a random number between 1 and 4 and append it into the num list. Then it will show the sequence of colors using the function show. Then it will go through and verify if what is touched on the board correlates to what is on the list, if it is correct, it will flash green and move on to the next round or it will flash red and end.
+Parameters: None
+Return: None
+‘’’
 def nextround():
     num = random.randint(1,4)
     gencolors.append(num)
@@ -95,6 +135,7 @@ def nextround():
         else:
             over = True
     time.sleep(.1)
+
 
 while True:
     if abutton.value == True:
